85dfa9806fce88698b2ab54e61677bb19b101fa0
mchafulumira <125554742+mc265@users.noreply.github.com>
2023-05-11 12:04:47 +0200

    Update export.js

--- export.js
+++ export.js
@@ -2,7 +2,10 @@
 Highcharts.getJSON('https://raw.githubusercontent.com/mc265/data/main/IRW%20export%20edited.json', function (data) {
   
   
-
+var formattedData = data.map(function (point) {
+    var date = new Date(point[0]);
+    return [Date.UTC(date.getFullYear(), 0, 1), point[1]];
+  });
      
 
     // Create the chart
