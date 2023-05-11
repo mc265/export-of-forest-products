c92e6172f8d6ab587cccb7f3babb41e2e41eebff
mchafulumira <125554742+mc265@users.noreply.github.com>
2023-05-11 14:18:06 +0200

    Update export.js

--- export.js
+++ export.js
@@ -11,9 +11,36 @@ Highcharts.getJSON('https://raw.githubusercontent.com/mc265/data/main/IRW%20expo
     // Create the chart
     Highcharts.stockChart('container', {
        
-        rangeSelector: {
-            selected: 3
-        },
+      rangeSelector: {
+    buttons: [{
+        type: 'year',
+        count: 1,
+        text: '1year',
+        events: {
+            click: function() {
+                alert('Clicked button');
+            }
+        }
+    }, {
+        type: 'month',
+        count: 3,
+        text: '3m'
+    }, {
+        type: 'month',
+        count: 6,
+        text: '6m'
+    }, {
+        type: 'ytd',
+        text: 'YTD'
+    }, {
+        type: 'year',
+        count: 1,
+        text: '1y'
+    }, {
+        type: 'all',
+        text: 'All'
+    }]
+},
 
         title: {
             text: 'Industrial Roundwood'
